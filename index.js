bde60d918f6ebe0b7b2b0831af3d1adc670fbb65
krishanbhag <krishna@wirecamp.com>
2017-11-11 22:24:10 +0530

    test heroku

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const bodyParser = require('body-parser')
 const request = require('request')
 
 const app = express()
-app.set('port',(process.env.PORT || 5000))
 
-//allow to process data
-app.use(bodyParser.urlencoded({extended:false}))
+app.set('port', (process.env.PORT || 5000))
+
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//Routes
-app.get('/',function(req,res) {
- res.send('Test')
+// ROUTES
 
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
 })
+
 let token = "EAAM6vqeZBUsQBAEh4lyEOpMCJqlIDVUYZB0CWRfecYjS9O1idlH9Bfdk2hlesM819k4sZC0BRQZBPcmfbRUM42X5e57zGUHhyYLrn9192eJNWP0ZAw1LQK6pWf78ZAZBGoE4eziVNdoQX90NckPqbI9rrdVZA2mneOrpivzdzDwWoQZDZD"
+
+// Facebook 
+
 app.get('/webhook/', function(req, res) {
 	if (req.query['hub.verify_token'] === "admin@1023") {
 		res.send(req.query['hub.challenge'])
@@ -26,14 +31,9 @@ app.get('/webhook/', function(req, res) {
 
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
-
-	sendText(sender, "Text echo: " + messaging_events[0])
-
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = messaging_events[i]
 		let sender = event.sender.id
-		console.log("**** sender Id" + sender)
-		console.log("**** Message" + event.message.text)
 		if (event.message && event.message.text) {
 			let text = event.message.text
 			sendText(sender, "Text echo: " + text.substring(0, 100))
@@ -54,13 +54,13 @@ function sendText(sender, text) {
 		}
 	}, function(error, response, body) {
 		if (error) {
-			console.log("**** sending error")
+			console.log("sending error")
 		} else if (response.body.error) {
-			console.log("**** response body error")
+			console.log("response body error")
 		}
 	})
 }
-app.listen(app.get('port'),function() {
- 
-  console.log("running port")
+
+app.listen(app.get('port'), function() {
+	console.log("running: port")
 })
